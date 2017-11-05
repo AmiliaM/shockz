0a3d4684d172bc07ee6d2fd63e34cd261216a780
amilia <macinta@clarkson.edu>
2017-11-05 13:17:39 -0500

    draw player on screen

--- shockz.py
+++ shockz.py
@@ -6,8 +6,18 @@ screen = pygame.display.set_mode(size)
 
 black = 0, 0, 0
 
+player = pygame.image.load("player.bmp")
+playerrect = player.get_rect()
+
 while 1:
+    #user input
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+    #turn and processing
+    print playerrect
+    
+    #draw
     screen.fill(black)
+    screen.blit(player, playerrect)
+    pygame.display.flip()
